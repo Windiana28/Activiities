ec103c6f8c11305ff8af2cede43580f161921e42
Yosua Hutabarat <yhutabarat24@gmail.com>
2023-02-03 00:58:19 +0700

    Add function find Rodo and find Todo Index

--- js/script.js
+++ js/script.js
@@ -4,3 +4,30 @@ const RENDER_EVENT = 'render-todo';
 function generateId() {
   return +new Date();
 }
+
+function generateTodoObject(id, task, timestamp, isCompleted) {
+  return {
+    id,
+    task,
+    timestamp,
+    isCompleted,
+  };
+}
+
+function findTodo(todoId) {
+  for (const todoItem of todos) {
+    if (todoItem.id === todoId) {
+      return todoItem;
+    }
+  }
+  return null;
+}
+
+function findTodoIndex(todoId) {
+  for (const index in todos) {
+    if (todos[index].id === todoId) {
+      return index;
+    }
+  }
+  return -1;
+}
